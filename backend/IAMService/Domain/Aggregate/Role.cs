072fe4a90d6da436d0dc42083d6a520ba93b0ca0
Tiến Phát <Tienphat040322@gmail.com>
2025-11-06 16:54:40 +0700

    user and role aggregate

---
+++ backend/IAMService/Domain/Aggregate/Role.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregate
+{
+    public class Role : BaseEntity
+    {
+        [Key]
+        public Guid RoleId { get; private set; } = Guid.NewGuid();
+        [Required]
+        public string RoleName { get; private set; } = string.Empty;
+        
+        private Role() { }
+        public Role(string roleName)
+        {
+            RoleName = roleName;
+        }
+    }
+}
