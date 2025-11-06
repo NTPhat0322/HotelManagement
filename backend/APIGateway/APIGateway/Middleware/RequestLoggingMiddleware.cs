aeb60836d47c598601225295cd8811b3e0631861
Tiến Phát <Tienphat040322@gmail.com>
2025-11-06 09:52:51 +0700

    config ocelot to apigateway

---
+++ backend/APIGateway/APIGateway/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,13 @@
+﻿namespace APIGateway.Middleware
+{
+    public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            logger.LogInformation($"{context.Request.Method} {context.Request.Path}");
+            await next(context);
+            logger.LogInformation($"{context.Response.StatusCode}");
+        }
+    }
+
+}
