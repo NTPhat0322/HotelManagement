ee36c2bbf6d3f6aa96f2ae9982250a2841f926f7
Tiến Phát <Tienphat040322@gmail.com>
2025-11-07 15:56:23 +0700

    more about middleware

--- backend/IAMService/API/Program.cs
+++ backend/IAMService/API/Program.cs
@@ -1,3 +1,8 @@
+using API.Middleware;
+using DotNetEnv;
+using Microsoft.OpenApi.Models;
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "API Gateway", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -16,8 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
+app.UseMiddleware<GlobalExceptionHandler>();
+app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ backend/IAMService/Application/Helpers/JwtHelper.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Domain.Aggregate;
+using System.Security.Claims;
+
+namespace Application.Helpers
+{
+    public static class JwtHelper
+    {
+        private static readonly string JwtKey = Environment.GetEnvironmentVariable("JWT_KEY")!;
+        private static readonly string JwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER")!;
+        private static readonly string JwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE")!;
+    
+        //public static string CreateToken(User user)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+
+        //    };
+        //}
+    }
+}
