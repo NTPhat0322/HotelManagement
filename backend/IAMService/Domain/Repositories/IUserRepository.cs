5a2e3bf1e32b3c2424e20cf8955378beded7556f
Tiến Phát <Tienphat040322@gmail.com>
2025-11-09 10:51:29 +0700

    modifying user repo

---
+++ backend/IAMService/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByPhoneNumber(string phoneNumber);
+    }
+}
