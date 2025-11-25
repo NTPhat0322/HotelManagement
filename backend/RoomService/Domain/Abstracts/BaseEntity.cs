dc3b92bcb600064f3dc332975740896b05e69255
Tiến Phát <Tienphat040322@gmail.com>
2025-11-25 10:10:57 +0700

    base for room service

---
+++ backend/RoomService/Domain/Abstracts/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Domain.Abstracts
+{
+    public abstract class BaseEntity
+    {
+        public bool IsDeleted { get; private set; } = false;
+        public DateTime? DeletedAt { get; private set; }
+        public string? DeletedBy { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public string? CreatedBy { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public string? UpdatedBy { get; private set; }
+    }
+}
---
+++ backend/RoomService/Domain/Aggregates/RoomType.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Aggregates
+{
+    public class RoomType : BaseEntity
+    {
+        [Key]
+        public Guid RoomTypeId { get; private set; } = Guid.NewGuid();
+        [Required]
+        public string Name { get; private set; } = string.Empty;
+        [Required]
+        public int MaxGuests { get; private set; }
+
+    }
+}
